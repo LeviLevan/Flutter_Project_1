c63482ad3c8f2c8163154edf9fe980ad32df55b2
DenisDevo <denys.misiura@extrawest.com>
2020-02-08 19:09:41 -0600

    Step One

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cources/screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Application());
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
